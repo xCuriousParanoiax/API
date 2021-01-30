e020708fcfcb8b8a32a2a1455c8914cc0ed0048d
xCuriousParanoiax <MuhamedRajaby_work@outlook.com>
2021-01-29 19:35:50 -0500

    Tweaked line counter

--- line_counter.py
+++ line_counter.py
@@ -5,7 +5,7 @@
 targetFile = "main.py"
 
 with open(targetFile) as f:
-	numberOfLines = len([l for l in f.readlines() if l.strip()])
+	numberOfLines = len([l for l in f.readlines() if l.strip() and not l.strip().startswith("#")])
 
 print(numberOfLines)
 
